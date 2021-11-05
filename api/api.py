b625e84347c86d561a739082467ff772fb5ec5f2
Fabio Bonelli <fb@fabiobonelli.it>
2021-11-05 19:31:34 +0100

    feat: implement confirmation endpoint

--- api/api.py
+++ api/api.py
@@ -6,22 +6,38 @@ import requests
 from sanic import Blueprint
 from sanic.response import json
 
+MAILGUN_KEY = os.environ.get('MAILGUN_KEY', '')
+JWT_KEY = os.environ.get('JWT_KEY', '')
+
 bp = Blueprint("api", url_prefix="/api")
 
-@bp.route('/date', methods=['GET'])
-async def get_ldate(request):
-    return json({'date': 'today!', 'secret': os.environ.get('SECRET', '')})
+@bp.route('/users', methods=['PUT'])
+async def confirm_user(req):
+    try:
+        signed_jwt = jwt.decode(req.json.get('jwt', ''), key=JWT_KEY, algorithms="HS256")
+    except jwt.exceptions.InvalidTokenError as exc:
+        return json({'message': f"{exc}"}, status=400)
+
+    address = signed_jwt['address']
+
+    res = requests.put(
+        f"https://api.eu.mailgun.net/v3/lists/newsletter@prossimapa.gov.it/members/{address}",
+        auth=('api', MAILGUN_KEY),
+        data={'subscribed': 'yes'},
+    )
+    if res.status_code != 200:
+        return json(res.json(), status=res.status_code)
+
+    return json({'message': 'ok'})
 
 @bp.route('/users', methods=['POST'])
 async def post_user(req):
-    mailgun_key = os.environ.get('MAILGUN_KEY', '')
-
     address = req.json.get('address', '')
     fields = ['representative', 'ente', 'enteSelect', 'message']
 
     res = requests.post(
         "https://api.eu.mailgun.net/v3/lists/newsletter@prossimapa.gov.it/members",
-        auth=('api', mailgun_key),
+        auth=('api', MAILGUN_KEY),
         data={'address': address,
               'vars': dumps({k: req.json.get(k, '') for k in fields}),
               'subscribed': 'no',
@@ -30,11 +46,11 @@ async def post_user(req):
     if res.status_code != 200:
         return json(res.json(), status=res.status_code)
 
-    signed_jwt = jwt.encode(payload={'address': address}, key=os.environ.get('JWT_KEY', ''))
+    signed_jwt = jwt.encode(payload={'address': address}, key=JWT_KEY)
 
     res = requests.post(
         "https://api.eu.mailgun.net/v3/prossimapa.gov.it/messages",
-        auth=('api', mailgun_key),
+        auth=('api', MAILGUN_KEY),
         data={'from': 'no-reply@prossimapa.gov.it',
               'to': address,
               'subject': 'TODO Blah blah conferma la registrazione',
